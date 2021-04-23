195512302bdb33eba41c709c306a72436d66701a
AMahdy98 <ahmadmahdy3098@gmail.com>
2021-04-23 23:51:24 +0200

    add Hough line

--- libs/Hough.py
+++ libs/Hough.py
@@ -1,11 +1,118 @@
 import cv2
 import numpy as np
+import math
 
+
+def hough_peaks(H, num_peaks, threshold=0, nhood_size=3):
+    """A function that returns the indicies of the accumulator array H that
+    correspond to a local maxima.  If threshold is active all values less
+    than this value will be ignored, if neighborhood_size is greater than
+    (1, 1) this number of indicies around the maximum will be surpessed."""
+    # loop through number of peaks to identify
+    indicies = []
+    H1 = np.copy(H)
+    for i in range(num_peaks):
+        idx = np.argmax(H1)  # find argmax in flattened array
+        H1_idx = np.unravel_index(idx, H1.shape)  # remap to shape of H
+        indicies.append(H1_idx)
+
+        # surpess indicies in neighborhood
+        idx_y, idx_x = H1_idx  # first separate x, y indexes from argmax(H)
+        # if idx_x is too close to the edges choose appropriate values
+        if (idx_x - (nhood_size / 2)) < 0:
+            min_x = 0
+        else:
+            min_x = idx_x - (nhood_size / 2)
+        if (idx_x + (nhood_size / 2) + 1) > H.shape[1]:
+            max_x = H.shape[1]
+        else:
+            max_x = idx_x + (nhood_size / 2) + 1
+
+        # if idx_y is too close to the edges choose appropriate values
+        if (idx_y - (nhood_size / 2)) < 0:
+            min_y = 0
+        else:
+            min_y = idx_y - (nhood_size / 2)
+        if (idx_y + (nhood_size / 2) + 1) > H.shape[0]:
+            max_y = H.shape[0]
+        else:
+            max_y = idx_y + (nhood_size / 2) + 1
+
+        # bound each index by the neighborhood size and set all values to 0
+        for x in range(min_x, max_x):
+            for y in range(min_y, max_y):
+                # remove neighborhoods in H1
+                H1[y, x] = 0
+
+                # highlight peaks in original H
+                if x == min_x or x == (max_x - 1):
+                    H[y, x] = 255
+                if y == min_y or y == (max_y - 1):
+                    H[y, x] = 255
+
+    # return the indicies and the original Hough space with selected points
+    return indicies, H
+
+
+def hough_lines_draw(img, indicies, rhos, thetas):
+    """A function that takes indicies a rhos table and thetas table and draws
+    lines on the input images that correspond to these values."""
+    for i in range(len(indicies)):
+        # reverse engineer lines from rhos and thetas
+        rho = rhos[indicies[i][0]]
+        theta = thetas[indicies[i][1]]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        # these are then scaled so that the lines go off the edges of the image
+        x1 = int(x0 + 1000 * (-b))
+        y1 = int(y0 + 1000 * (a))
+        x2 = int(x0 - 1000 * (-b))
+        y2 = int(y0 - 1000 * (a))
+
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+def line_detection(source: np.ndarray):
+
+    img = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+    img = cv2.GaussianBlur(img, (5, 5), 1.5)
+    img = cv2.Canny(img, 100, 200)
+
+    # Rho and Theta ranges
+    thetas = np.deg2rad(np.arange(-90.0, 90.0))
+    width, height = img.shape
+    diag_len = math.ceil(np.sqrt(np.square(width) + np.square(height)))  # max_dist
+    rhos = np.linspace(-diag_len, diag_len, diag_len * 2)
+
+    # Cache some resuable values
+    cos_t = np.cos(thetas)
+    sin_t = np.sin(thetas)
+    num_thetas = len(thetas)
+
+    # Hough accumulator array of theta vs rho
+    accumulator = np.zeros((2 * diag_len, num_thetas), dtype=np.uint64)
+    y_idxs, x_idxs = np.nonzero(img)  # (row, col) indexes to edges
+
+    # Vote in the hough accumulator
+    for i in range(len(x_idxs)):
+        x = x_idxs[i]
+        y = y_idxs[i]
+
+        for t_idx in range(num_thetas):
+            # Calculate rho. diag_len is added for a positive index
+            rho = int(round(x * cos_t[t_idx] + y * sin_t[t_idx]) + diag_len)
+            accumulator[rho, t_idx] += 1
+
+    return accumulator, rhos, thetas
 
 def hough_lines(source: np.ndarray) -> np.ndarray:
-    # TODO Apply hough lines detection algorithm
-    pass
 
+    H, rhos, thetas = line_detection(source)
+    indicies, H = hough_peaks(H, 3, nhood_size=11) # find peaks
+    hough_lines_draw(source, indicies, rhos, thetas)
+
+    return source
 
 def hough_circles(source: np.ndarray, radius: int = 25) -> np.ndarray:
     # TODO Apply hough circles detection algorithm

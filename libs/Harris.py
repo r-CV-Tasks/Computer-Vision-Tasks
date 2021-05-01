9201cc1ff91a15738f1cd181113888e793a8f1df
Refaey <abdullah.m.alrefaey@gmail.com>
2021-05-01 08:05:31 +0200

    Empty Spaces

--- libs/Harris.py
+++ libs/Harris.py
@@ -1,9 +1,21 @@
 import cv2
 import numpy as np
 
+
 def apply_harris_operator(source: np.ndarray) -> np.ndarray:
     """
 
+    :param source:
+    :return:
+    """
 
+    return source
+
+
+def get_harris_corners(source: np.ndarray):
+    """
+
+    :param source:
+    :return:
     """
     return source

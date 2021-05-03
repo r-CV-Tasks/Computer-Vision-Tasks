6dc4608d3af3521939e317d1a2a2bf007dbf938f
Refaey <abdullah.m.alrefaey@gmail.com>
2021-05-04 01:16:04 +0200

    Refactor Feature Matching Functions

--- libs/FeatureMatching.py
+++ libs/FeatureMatching.py
@@ -1,22 +1,11 @@
 import cv2
 import numpy as np
-from libs.Harris import apply_harris_operator2
 import matplotlib.pyplot as plt
+from typing import Callable
 
 
-def apply_feature_matching(desc1: np.ndarray, desc2: np.ndarray) -> np.ndarray:
-    """
-
-    :param desc1:
-    :param desc2:
-    :return:
-    """
-
-    out = desc1
-
-    return out
-
-def match_features(desc1: np.ndarray, desc2: np.ndarray, calculator) -> list:
+def apply_feature_matching(desc1: np.ndarray, desc2: np.ndarray,
+                           match_calculator: Callable[[list, list], float]) -> list:
     """
 
     Perform feature matching between 2 feature descriptors
@@ -25,14 +14,11 @@ def match_features(desc1: np.ndarray, desc2: np.ndarray, calculator) -> list:
                   Dimensions: rows (number of key points) x columns (dimension of the feature descriptor i.e: 128)
     :param desc2: The feature descriptors of image 2.
                   Dimensions: rows (number of key points) x columns (dimension of the feature descriptor i.e: 128)
-    :param calculator: A function to use in matching features
+    :param match_calculator: A Callable function to use in matching features:
+                        - calculate_ssd (Sum Square Distance)
+                        - calculate_ncc (Normalized Cross Correlation)
 
-    :return:
-        features matches, a list of cv2.DMatch objects:
-            How to set attributes:
-                - queryIdx: The index of the feature in the first image
-                - trainIdx: The index of the feature in the second image
-                - distance: The distance between the two features
+    :return: features matches, a list of cv2.DMatch objects
     """
 
     # Check descriptors dimensions are 2
@@ -47,29 +33,41 @@ def match_features(desc1: np.ndarray, desc2: np.ndarray, calculator) -> list:
         return []
 
     # number of key points in each image
-    numKeyPoints1 = desc1.shape[0]
-    numKeyPoints2 = desc2.shape[0]
+    num_key_points1 = desc1.shape[0]
+    num_key_points2 = desc2.shape[0]
 
     # List to store the matches scores
     matches = []
 
     # Loop over each key point in image1
     # We need to calculate similarity with each key point in image2
-    for kp1 in range(numKeyPoints1):
+    for kp1 in range(num_key_points1):
         # Initial variables which will be updated in the loop
         distance = -np.inf
         y_index = -1
 
         # Loop over each key point in image2
-        for kp2 in range(numKeyPoints2):
+        for kp2 in range(num_key_points2):
 
             # Match features between the 2 vectors
-            value = calculator(desc1[kp1], desc2[kp2])
+            value = match_calculator(desc1[kp1], desc2[kp2])
+
+            # SSD values examples: (50, 200, 70), we need to minimize SSD (Choose 50)
+            # In case of SSD matching: (value is returned as a "negative" number) (-50, -200, -70)
+            # So we compare it with -np.inf. (The sorting will be reversed later)
+
+            # NCC values examples: (0.58, 0.87, 0.42), we need to maximize NCC (Choose 0.87)
+            # In case of NCC matching: (value is returned as a "positive" number) (-58, -0.87, -0.42)
+            # So we compare it with -np.inf. (The sorting will be reversed later)
 
             if value > distance:
                 distance = value
                 y_index = kp2
 
+        # Create a cv2.DMatch object for each match and set attributes:
+            # queryIdx: The index of the feature in the first image
+            # trainIdx: The index of the feature in the second image
+            # distance: The distance between the two features
         cur = cv2.DMatch()
         cur.queryIdx = kp1
         cur.trainIdx = y_index
@@ -77,6 +75,7 @@ def match_features(desc1: np.ndarray, desc2: np.ndarray, calculator) -> list:
         matches.append(cur)
 
     return matches
+
 
 def calculate_ssd(desc1: list, desc2: list) -> float:
     """
@@ -95,16 +94,16 @@ def calculate_ssd(desc1: list, desc2: list) -> float:
     :return: A float number represent the SSD between two features vectors
     """
 
-    sumSquare = 0
+    sum_square = 0
 
     # Get SSD between the 2 vectors
     for m in range(len(desc1)):
-        sumSquare += (desc1[m] - desc2[m]) ** 2
+        sum_square += (desc1[m] - desc2[m]) ** 2
 
     # The (-) sign here because the condition we applied after this function call is reversed
-    sumSquare = - (np.sqrt(sumSquare))
+    sum_square = - (np.sqrt(sum_square))
 
-    return sumSquare
+    return sum_square
 
 
 def calculate_ncc(desc1: list, desc2: list) -> float:
@@ -139,77 +138,26 @@ def calculate_ncc(desc1: list, desc2: list) -> float:
 
 # TODO # Unfinished function
 def calculate_rssd(desc1, desc2):
-    sumSquare = 0
+    """
+    This function is responsible for:
+        - Calculating the ratio of the SSD distance of the two best matches
+        - Matching a feature in the first image with the closest feature in the second image.
 
-    for m in range(len(desc1)):
-        sumSquare += (desc1[m] - desc2[m]) ** 2
-    sumSquare = np.sqrt(sumSquare)
+    Note:
+        - multiple features from the first image may match the same feature in the second image.
+        - You don't need to threshold matches in this function
 
-
-
-def matchFeatures_SSD(desc1, desc2):
-    """'''
-    Input:
-        desc1 -- the feature descriptors of image 1 stored in a numpy array,
-            dimensions: rows (number of key points) x
-            columns (dimension of the feature descriptor)
-        desc2 -- the feature descriptors of image 2 stored in a numpy array,
-            dimensions: rows (number of key points) x
-            columns (dimension of the feature descriptor)
-    Output:
-        features matches: a list of cv2.DMatch objects
-            How to set attributes:
-                queryIdx: The index of the feature in the first image
-                trainIdx: The index of the feature in the second image
-                distance: The distance between the two features
+    :param desc1: The feature descriptor vector of one key point in image1.
+                  Dimensions: rows (1) x columns (dimension of the feature descriptor i.e: 128)
+    :param desc2: The feature descriptor vector of one key point in image2.
+                  Dimensions: rows (1) x columns (dimension of the feature descriptor i.e: 128)
+    :return: A float number represent the SSD between two features vectors
     """
 
-    # feature count = n
-    assert desc1.ndim == 2, "dimensions are not equal"
-    # feature count = m
-    assert desc2.ndim == 2
-    # the two features should have the type
-    assert desc1.shape[1] == desc2.shape[1]
-
-    if desc1.shape[0] == 0 or desc2.shape[0] == 0:
-        return []
-
-    # TODO 7: Perform simple feature matching.  This uses the SSD
-    # distance between two feature vectors, and matches a feature in
-    # the first image with the closest feature in the second image.
-    # Note: multiple features from the first image may match the same
-    # feature in the second image.
-    # TODO-BLOCK-BEGIN
-    numKeyPoints1 = desc1.shape[0]
-    numKeyPoints2 = desc2.shape[0]
-    matches = []
-
-    for x in range(numKeyPoints1):
-        # distance = -1
-        distance = np.inf
-        y_ind = -1
-        for y in range(numKeyPoints2):
-            sumSquare = 0
-            for m in range(desc1.shape[1]):
-                sumSquare += (desc1[x][m] - desc2[y][m]) ** 2
-
-            sumSquare = np.sqrt(sumSquare)
-
-            if sumSquare < distance:
-                distance = sumSquare
-                y_ind = y
-
-        cur = cv2.DMatch()
-        cur.queryIdx = x
-        cur.trainIdx = y_ind
-        cur.distance = distance
-        matches.append(cur)
-    # TODO-BLOCK-END
-
-    return matches
+    raise NotImplementedError
 
 
-def matchFeatures_Ratio_Test(desc1, desc2):
+def match_features_ratio_test(desc1, desc2):
     """"
     Input:
         desc1 -- the feature descriptors of image 1 stored in a numpy array,
@@ -226,7 +174,6 @@ def matchFeatures_Ratio_Test(desc1, desc2):
                 distance: The ratio test score
     """
 
-    matches = []
     # feature count = n
     assert desc1.ndim == 2
     # feature count = m
@@ -237,90 +184,30 @@ def matchFeatures_Ratio_Test(desc1, desc2):
     if desc1.shape[0] == 0 or desc2.shape[0] == 0:
         return []
 
-    # TODO 8: Perform ratio feature matching.
-    # This uses the ratio of the SSD distance of the two best matches
-    # and matches a feature in the first image with the closest feature in the
-    # second image.
-    # Note: multiple features from the first image may match the same
-    # feature in the second image.
-    # You don't need to threshold matches in this function
-    # TODO-BLOCK-BEGIN
-    numKeyPoints1 = desc1.shape[0]
-    numKeyPoints2 = desc2.shape[0]
+    num_key_points1 = desc1.shape[0]
+    num_key_points2 = desc2.shape[0]
     matches = []
 
-    for x in range(numKeyPoints1):
+    for x in range(num_key_points1):
         distance1 = -1
         distance2 = -1
         y_ind = -1
-        for y in range(numKeyPoints2):
-            sumSquare = 0
+        for y in range(num_key_points2):
+            sum_square = 0
             for m in range(desc1.shape[1]):
-                sumSquare += (desc1[x][m] - desc2[y][m]) ** 2
-            sumSquare = np.sqrt(sumSquare)
-            if distance1 < 0 or (sumSquare < distance1 and distance1 >= 0):
+                sum_square += (desc1[x][m] - desc2[y][m]) ** 2
+            sum_square = np.sqrt(sum_square)
+            if distance1 < 0 or (sum_square < distance1 and distance1 >= 0):
                 distance2 = distance1
-                distance1 = sumSquare
+                distance1 = sum_square
                 y_ind = y
-            elif distance2 < 0 or (sumSquare < distance2 and distance2 >= 0):
-                distance2 = sumSquare
+            elif distance2 < 0 or (sum_square < distance2 and distance2 >= 0):
+                distance2 = sum_square
         cur = cv2.DMatch()
         cur.queryIdx = x
         cur.trainIdx = y_ind
         cur.distance = distance1 / distance2
         matches.append(cur)
-    # TODO-BLOCK-END
-
-    return matches
-
-
-def normalized_cross_correlation(desc1, desc2):
-    # feature count = n
-    assert desc1.ndim == 2
-    # feature count = m
-    assert desc2.ndim == 2
-    # the two features should have the type
-    assert desc1.shape[1] == desc2.shape[1]
-
-    if desc1.shape[0] == 0 or desc2.shape[0] == 0:
-        return []
-
-    # TODO 7: Perform simple feature matching.  This uses the SSD
-    # distance between two feature vectors, and matches a feature in
-    # the first image with the closest feature in the second image.
-    # Note: multiple features from the first image may match the same
-    # feature in the second image.
-    # TODO-BLOCK-BEGIN
-    numKeyPoints1 = desc1.shape[0]
-    numKeyPoints2 = desc2.shape[0]
-
-    print(f"kp1: {numKeyPoints1}")
-    print(f"kp2: {numKeyPoints2}")
-
-    matches = []
-
-    for x in range(numKeyPoints1):
-        distance = -1
-        y_ind = -1
-        for y in range(numKeyPoints2):
-
-            out1_norm = (desc1[x] - np.mean(desc1[x])) / (np.std(desc1[x]))
-            out2_norm = (desc2[y] - np.mean(desc2[y])) / (np.std(desc2[y]))
-
-            corr = np.mean(np.multiply(out1_norm, out2_norm))
-
-            # corr = np.mean(np.multiply(out1_zero_mean, out2_zero_mean)) / (out1_std * out2_std)
-            if corr > distance:
-                distance = corr
-                y_ind = y
-
-        cur = cv2.DMatch()
-        cur.queryIdx = x
-        cur.trainIdx = y_ind
-        print(f"distance: {distance}")
-        cur.distance = distance
-        matches.append(cur)
-    # TODO-BLOCK-END
 
     return matches
 
@@ -332,8 +219,6 @@ def main():
 
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-
-    # pip install opencv-contrib-python
 
     # sift
     sift = cv2.SIFT_create()
@@ -347,33 +232,17 @@ def main():
     # matches = bf.match(descriptors_1, descriptors_2)
     # matches = sorted(matches, key=lambda x: x.distance)
 
-    # matches_2 = matchFeatures_SSD(descriptors_1, descriptors_2)
-    # matches_2 = sorted(matches_2, key=lambda x: x.distance)
+    matches = apply_feature_matching(descriptors_1, descriptors_2, calculate_ncc)
+    matches = sorted(matches, key=lambda x: x.distance, reverse=True)
 
-    # matches_3 = matchFeatures_Ratio_Test(descriptors_1, descriptors_2)
-    # matches_3 = sorted(matches_3, key=lambda x: x.distance)
+    print(f"matches:\n {matches}")
+    print(f"matches shape {len(matches)}")
 
-    # matches_4 = normalized_cross_correlation(descriptors_1, descriptors_2)
+    matched_image = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2,
+                                    matches[:30], img2, flags=2)
 
-    matches_4 = match_features(descriptors_1, descriptors_2, calculate_ncc)
-    matches_4 = sorted(matches_4, key=lambda x: x.distance, reverse=True)
-
-    # print(f"matches_4 {matches_4}")
-    # print(f"matches_4.shape {len(matches_4)}")
-
-    # img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2,
-    #                        matches[:30], img2, flags=2)
-    # img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2,
-    #                        matches_2[:30], img2, flags=2)
-    # img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2,
-    #                        matches_3[:30], img2, flags=2)
-
-    img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2,
-                           matches_4[:30], img2, flags=2)
-
-    plt.imshow(img3), plt.show()
-
-    # matched_image = apply_feature_matching()
+    plt.imshow(matched_image)
+    plt.show()
 
 
 if __name__ == "__main__":
